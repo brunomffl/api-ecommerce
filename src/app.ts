da3a666faac816a4b2d11e03857f0bf535827637
brunomffl <brunocardoso07@Outlook.com>
2025-08-13 23:02:14 -0300

    feat: creating the routes structure

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
-import exp from "constants";
 import express from "express";
+import { routes } from "./routes";
 
 const app = express();
 
 app.use(express.json());
+
+app.use(routes);
 
 
 export { app };
